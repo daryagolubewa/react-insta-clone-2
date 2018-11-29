9d69e4321be80ba4af56fd903117ccdbb8f690ae
Alex Vainer <john.black.bee@gmail.com>
2018-11-29 17:11:10 +0300

    Added sceleton for react,express

---
+++ src/client/components/app/app.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Type from 'prop-types';
+import { Link } from 'react-router-dom';
+import elbrusImg from './elbrus.png';
+import { PAGES } from '../../routes/pages';
+import { bemClassNameFactory } from '../../utils/bem';
+import './app.css';
+import appRoutes from '../../routes/routes'
+
+
+const cn = bemClassNameFactory('app');
+ 
+export default class App extends Component {
+  static propTypes = {
+    appName: Type.string
+  };
+
+  static defaultProps = {
+    appName: 'Default Name'
+  };
+
+  state = {
+    user: null
+  };
+
+  handleSetUser = (user) => {
+    this.setState({user})
+  } 
+
+
+  handleClickButton = () => {
+    const { buttonActive } = this.state;
+    console.log(buttonActive);
+    this.setState({ buttonActive: !buttonActive });
+  };
+
+  componentDidMount() {
+    const fetchFunc = async () => {
+      const res = await fetch('/api/test');
+      console.log(res);
+      return res;
+    };
+    fetchFunc();
+  }
+
+  render() {
+    const { appName } = this.props;
+    console.log(this.props);
+    return (
+      <div className={ cn() }>
+        <div>
+          <Link to={PAGES.login.path}>Log in</Link>
+          {/* <Link to={}></Link> */}
+        </div>
+        { appRoutes(this.state.user, this.handleSetUser) }
+      </div>
+    );
+  }
+}
---
+++ src/client/routes/pages.js
@@ -0,0 +1,14 @@
+export const PAGES = {
+  home: {
+    name: 'home',
+    path: '/'
+  },
+  login: {
+    name: 'login',
+    path: '/login'
+  },
+  page404: {
+    name: 'page404',
+    path: '/page404'
+  }
+};
