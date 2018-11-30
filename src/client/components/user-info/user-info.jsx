752fe294a4d0a1f4bd618f65b2093a47145729a3
daryagolubeva <darya.golubewa@gmail.com>
2018-11-30 12:02:25 +0300

    user-info component edited;

--- src/client/components/user-info/user-info.jsx
+++ src/client/components/user-info/user-info.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import './user-info.css';
 import logo from './user-default-image.png';
-
+import settings from './user-settings.svg';
 
 export default class UserInfo extends Component {
   render() {
     return (
             <div className='user-info__box'>
+
                 <div className='user-info_photo'>
                     <img src={logo} />
                 </div>
+
                 <div className='user-info__connections'>
-                    <div className='user-name'> Batman Batmanovich</div>
-                    <div className='user-info__info'>
-                        I am Batman!</div>
+                    <div className='user-info'>
+                        <div className='user-info-info'>
+                            <div className='user-name'> Batman Batmanovich</div>
+                            <div className='user-description'> I am Batman!</div>
+                        </div>
+                    <div className='info-settings'>
+                        <img src={settings} className='settings-pic'/>
+                    </div>
+                    </div>
                     <div className='user-info__statistics'>
                         <div className='user-posts'> 0 posts</div>
                         <div className='user-followers'> 0 followers</div>
@@ -21,7 +29,6 @@ export default class UserInfo extends Component {
                     </div>
                 </div>
             </div>
-
     );
   }
 }
